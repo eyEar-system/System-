9139d933e571aa6218842f3e91b27378a8c394a0
eyEar-system <eyearassistant@gmail.com>
2025-03-23 22:32:03 +0200

    Create module2.py

---
+++ module2.py
@@ -0,0 +1,9 @@
+class ClassTwo:
+    def __init__(self, age=18):
+        self.age = age
+
+    def show_age(self):
+        return f"You are {self.age} years old."
+
+    def multiply(self, a, b):
+        return a * b
